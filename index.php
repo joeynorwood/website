2bb37201d94041f0657d6e76e63b5be73000afa4
joesephnorwood <norwoo25@msu.edu>
2018-05-04 15:52:08 -0500

    added github link

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ echo $htmlGen->createPageBeforeContent('Home');
 
 <h4>Welcome and Enjoy</h4>
 <p>Here you will find information about me: Joey Norwood</p>
-    
+<p>See code on my Github at:&nbsp;<a href='https://github.com/joeynorwood/website' target='_blank'>Link</a></p>
 
 <?php
 echo $htmlGen->createPageAfterContent();
